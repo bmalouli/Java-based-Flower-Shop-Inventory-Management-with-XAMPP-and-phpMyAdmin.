b3384cae8dfb9e6223add08a8b6462612aac0159
n4dz <nadzamini@gmail.com>
2023-08-24 10:44:21 -0400

    Ajout du rechercher avec le NOM

--- src/RechercherFleurFrame.java
+++ src/RechercherFleurFrame.java
@@ -8,6 +8,7 @@ public class RechercherFleurFrame extends JFrame {
 
     private JPanel contentPane;
     private JTextField textField;
+    private JTextField textField_1;
 
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
@@ -25,17 +26,12 @@ public class RechercherFleurFrame extends JFrame {
     public RechercherFleurFrame() {
     	setTitle("Rechercher une fleur");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 800, 600);
+        setBounds(100, 100, 800, 600); // Définir la taille du JFrame
         contentPane = new JPanel();
         contentPane.setBackground(new Color(243, 231, 230));
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
         setContentPane(contentPane);
         contentPane.setLayout(null);
-        
-        JLabel lblNewLabel = new JLabel("Rechercher avec le ID:");
-        lblNewLabel.setForeground(new Color(182, 134, 111));
-        lblNewLabel.setBounds(121, 152, 143, 16);
-        contentPane.add(lblNewLabel);
         
         JLabel lblNom = new JLabel("NOM");
         lblNom.setForeground(new Color(182, 134, 111));
@@ -69,7 +65,7 @@ public class RechercherFleurFrame extends JFrame {
         contentPane.add(btnNewButton);
         
         textField = new JTextField();
-        textField.setBounds(292, 147, 210, 26);
+        textField.setBounds(304, 96, 210, 26);
         contentPane.add(textField);
         textField.setColumns(10);
         
@@ -79,22 +75,42 @@ public class RechercherFleurFrame extends JFrame {
         	}
         });
         btnAnnuler.setForeground(new Color(167, 116, 117));
-        btnAnnuler.setBounds(538, 160, 143, 29);
+        btnAnnuler.setBounds(596, 214, 143, 29);
         contentPane.add(btnAnnuler);
         
-        JButton btnRechercher = new JButton("Rechercher");
+        JButton btnRechercher = new JButton("Rechercher selon ID");
         btnRechercher.setForeground(new Color(167, 116, 117));
-        btnRechercher.setBounds(538, 119, 143, 29);
+        btnRechercher.setBounds(538, 96, 170, 29);
         contentPane.add(btnRechercher);
         
         JLabel lblInformationsSurLa = new JLabel("Informations sur la fleur recherché :");
         lblInformationsSurLa.setForeground(new Color(182, 134, 111));
-        lblInformationsSurLa.setBounds(254, 239, 248, 16);
+        lblInformationsSurLa.setBounds(266, 253, 248, 16);
         contentPane.add(lblInformationsSurLa);
         
         JLabel lblId = new JLabel("ID");
         lblId.setForeground(new Color(182, 134, 111));
         lblId.setBounds(68, 294, 30, 16);
         contentPane.add(lblId);
+        
+        JCheckBox chckbxNewCheckBox = new JCheckBox("Rechercher avec le ID:");
+        chckbxNewCheckBox.setForeground(new Color(180, 143, 124));
+        chckbxNewCheckBox.setBounds(101, 97, 170, 23);
+        contentPane.add(chckbxNewCheckBox);
+        
+        JCheckBox chckbxRechercherAvecLe = new JCheckBox("Rechercher avec le NOM:");
+        chckbxRechercherAvecLe.setForeground(new Color(180, 143, 124));
+        chckbxRechercherAvecLe.setBounds(101, 151, 187, 23);
+        contentPane.add(chckbxRechercherAvecLe);
+        
+        textField_1 = new JTextField();
+        textField_1.setColumns(10);
+        textField_1.setBounds(304, 150, 210, 26);
+        contentPane.add(textField_1);
+        
+        JButton btnRechercherSelonNom = new JButton("Rechercher selon NOM");
+        btnRechercherSelonNom.setForeground(new Color(167, 116, 117));
+        btnRechercherSelonNom.setBounds(538, 150, 170, 29);
+        contentPane.add(btnRechercherSelonNom);
     }
 }
